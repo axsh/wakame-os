850ede53c6d5107753f3eb0e82c4190c8d47421b
Yasuhiro Yamazaki <y-yamazaki@axsh.net>
2010-09-07 20:38:08 +0900

    Add base class for remote access.

---
+++ lib/clios/remote.rb
@@ -0,0 +1,25 @@
+module WakameOS
+  module Client
+    class SetupBase
+      @@credential = {}
+      @@spec_name  = 'default'
+      @@option     = {}
+      
+      def self.credential;              @@credential;                  end
+      def self.credential=(credential); @@credential = credential.dup; end
+      def self.spec_name;               @@spec_name;                   end
+      def self.spec_name=(spec_name);   @@spec_name = spec_name.dup;   end
+      
+      def self.setup(option={})
+        credential = option.delete(:credential) || @@credential
+        spec_name  = option.delete(:spec_name)  || @@spec_name  || 'default'
+        
+        option = option.dup.merge({:spec => '08'})
+        @@credential = credential.dup
+        @@spec_name  = spec_name.dup
+        @@option     = option
+        {:credential => credential.dup, :spec_name => spec_name.dup}
+      end
+    end
+  end
+end
