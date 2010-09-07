de59e52d4bd84ac6a116b599c962c0f26cd2cc64
Yasuhiro Yamazaki <y-yamazaki@axsh.net>
2010-09-07 21:36:58 +0900

    Add shared queue class.

---
+++ lib/clios/remote_queue.rb
@@ -0,0 +1,63 @@
+#
+require 'clios'
+require 'bunny'
+
+module WakameOS
+  module Client
+    class Queue < SetupBase
+
+      def self.setup(option={})
+        hash = super(option)
+        remote = Remote.new(option)
+        return remote
+      end
+
+      class Remote
+        attr_reader :prefix, :option
+        def initialize(option)
+          @mutex  = Monitor.new
+          @prefix = option.delete(:prefix) || 'wakame.user.queue.'
+          @name   = option.delete(:name  ) || WakameOS::Utility::UniqueKey.new
+          @option = option
+          @bunny_option = { :spec => '08' } # TODO: other server
+
+          WakameOS::Client::SystemCall.instance.add_gc_target_queues(@prefix+@name)
+
+        end
+
+        def finalize
+          amqp = Bunny.new(@bunny_option)
+          amqp.start
+          amqp.queue(@prefix+@name, :auto_delete => true, :exclusive => false).delete
+          amqp.stop
+
+          WakameOS::Client::SystemCall.instance.remove_gc_target_queues(@prefix+@name)
+        end
+
+        def push(object)
+          amqp = Bunny.new(@bunny_option)
+          amqp.start
+          amqp.queue(@prefix+@name, :auto_delete => true, :exclusive => false).publish(::Marshal.dump(object))
+          amqp.stop
+        end
+
+        def pop(wait=true)
+          amqp = Bunny.new(@bunny_option)
+          amqp.start
+          queue = amqp.queue(@prefix+@name, :auto_delete => true, :exclusive => false)
+          begin
+            data = queue.pop
+            loop_flag = (data[:payload]==:queue_empty)
+            sleep 0.1 if loop_flag # TODO: lazy...
+          end while loop_flag
+          ret = ::Marshal.load(data[:payload])
+          amqp.stop
+          ret
+        end
+        
+      end
+    end
+  end
+end
+
+      
