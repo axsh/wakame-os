63790282dca058acb1f2b30a0ad8aa9a9eb73e2b
Yasuhiro Yamazaki <y-yamazaki@axsh.net>
2010-09-10 22:26:19 +0900

    to use defailt credential

--- lib/clios/remote.rb
+++ lib/clios/remote.rb
@@ -1,9 +1,9 @@
 module WakameOS
   module Client
     class SetupBase
-      @@credential = {}
+      @@credential = nil
       @@spec_name  = 'default'
-      @@option     = {}
+      @@option     = nil
       
       def self.credential;              @@credential;                  end
       def self.credential=(credential); @@credential = credential.dup; end
@@ -11,8 +11,8 @@ module WakameOS
       def self.spec_name=(spec_name);   @@spec_name = spec_name.dup;   end
       
       def self.setup(option={})
-        credential = option.delete(:credential) || @@credential
-        spec_name  = option.delete(:spec_name)  || @@spec_name  || 'default'
+        credential = option.delete(:credential) || @@credential || Environment.os_user_credential
+        spec_name  = option.delete(:spec_name)  || @@spec_name  || Environment.os_default_spec_name
         
         option = option.dup
         @@credential = credential.dup
